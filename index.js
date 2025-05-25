c81be388944cb9c8c39158ff3ffd012c9d1acf8c
maheshreddi7 <maheshreddy.pannalaa@gmail.com>
2025-05-25 23:24:28 +0530

    added reset option

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
-/**
-Challenge: 
+let postsArray = []
+const titleInput = document.getElementById("post-title")
+const bodyInput = document.getElementById("post-body")
+const form = document.getElementById("new-post")
 
-When the button is clicked, call out to the Bored API
-(URL: https://apis.scrimba.com/bored/api/activity)
-and replace the h4 with the activity from the API
+function renderPosts() {
+    let html = ""
+    for (let post of postsArray) {
+        html += `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            <hr />
+        `
+    }
+    document.getElementById("blog-list").innerHTML = html
+}
 
-*/
-document.getElementById("get-activity").addEventListener("click", function() {
-  fetch("https://apis.scrimba.com/bored/api/activity")
-    .then(response => response.json())
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+    .then(res => res.json())
     .then(data => {
-      document.getElementById("activity").textContent = data.activity
+        postsArray = data.slice(0, 5)
+        renderPosts()
     })
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault()
+    const postTitle = titleInput.value
+    const postBody = bodyInput.value
+    const data = {
+        title: postTitle,
+        body: postBody
+    }
+    
+    const options = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+        .then(res => res.json())
+        .then(post => {
+            postsArray.unshift(post)
+            renderPosts()
+            /**
+             * Challenge: clear the form out!
+             */
+            titleInput.value = ""
+            bodyInput.value = ""
+            // form.reset()
+        })
 })
